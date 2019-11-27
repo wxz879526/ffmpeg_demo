677231227815ec825c94e89c403e0de60bf4fcee
My Name <my-name@chromium.org>
2019-11-27 12:21:12 +0800

    update

--- ffmpeg_demo/ffmpeg_demo.cpp
+++ ffmpeg_demo/ffmpeg_demo.cpp
@@ -36,6 +36,7 @@ static bool ffmpeg_image_reformat_frame(ffmpeg_image *info, AVFrame *frame, uint
 	{
 		if (lineSize != frame->linesize[0])
 		{
+			// frame->linesize 可能包含为了cpu性能而额外添加的填充数据
 			int min_size = lineSize < frame->linesize[0] ? lineSize : frame->linesize[0];
 
 			for (int y = 0; y < info->cy; ++y)
@@ -50,6 +51,7 @@ static bool ffmpeg_image_reformat_frame(ffmpeg_image *info, AVFrame *frame, uint
 	}
 	else
 	{
+		// 格式转换
 		sws_ctx = sws_getContext(info->cx, info->cy, info->format, info->cx,
 			info->cy, AV_PIX_FMT_BGRA, SWS_POINT, NULL, NULL, NULL);
 		if (!sws_ctx)
@@ -63,7 +65,7 @@ static bool ffmpeg_image_reformat_frame(ffmpeg_image *info, AVFrame *frame, uint
 		if (ret < 0)
 			return false;
 
-		info->format = AV_PIX_FMT_RGBA;
+		info->format = AV_PIX_FMT_BGRA;
 		return true;
 	}
 
@@ -82,23 +84,23 @@ int main(int argc, char *argv[])
 
 	ffmpeg_image info;
 	info.strFile = "123.png";
-	ret = avformat_open_input(&info.fmt_ctx, info.strFile.c_str(), NULL, NULL);
+	ret = avformat_open_input(&info.fmt_ctx, info.strFile.c_str(), NULL, NULL); //打开文件
 	if (ret < 0)
 		return -1;
 
-	ret = avformat_find_stream_info(info.fmt_ctx, NULL);
+	ret = avformat_find_stream_info(info.fmt_ctx, NULL); //找到文件的流信息
 	if (ret < 0)
 		return -1;
 
-	ret = av_find_best_stream(info.fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, 1, NULL, 0);
+	ret = av_find_best_stream(info.fmt_ctx, AVMEDIA_TYPE_VIDEO, -1, 1, NULL, 0); //找到最匹配的流
 	if (ret < 0)
 		return -1;
 
 	info.stream_idx = ret;
 	info.stream = info.fmt_ctx->streams[ret];
 	info.decoder_ctx = info.stream->codec;
-	info.decoder = avcodec_find_decoder(info.decoder_ctx->codec_id);
-	ret = avcodec_open2(info.decoder_ctx, info.decoder, NULL);
+	info.decoder = avcodec_find_decoder(info.decoder_ctx->codec_id); //找到解码器
+	ret = avcodec_open2(info.decoder_ctx, info.decoder, NULL); //用解码器读取文件信息
 	if (ret < 0)
 		return -1;
 	info.cx = info.decoder_ctx->width;
@@ -112,13 +114,13 @@ int main(int argc, char *argv[])
 	bool success = false;
 	AVFrame *frame = av_frame_alloc();
 	int got_frame = 0;
-	ret = av_read_frame(info.fmt_ctx, &packet);
+	ret = av_read_frame(info.fmt_ctx, &packet); //解复用得到AVPacket
 	while (!got_frame)
 	{
 #if LIBAVFORMAT_VERSION_INT >= AV_VERSION_INT(57, 40, 101)
-		ret = avcodec_send_packet(info.decoder_ctx, &packet);
+		ret = avcodec_send_packet(info.decoder_ctx, &packet); //使用对应的解码器解码
 		if (0 == ret)
-			ret = avcodec_receive_frame(info.decoder_ctx, frame);
+			ret = avcodec_receive_frame(info.decoder_ctx, frame);//解码得到AVFrame
 		got_frame = (0 == ret);
 		if (ret == AVERROR_EOF || ret == AVERROR(EAGAIN))
 			ret = 0;
@@ -133,23 +135,42 @@ int main(int argc, char *argv[])
 
 	SDL_Renderer *sdlRender = SDL_CreateRenderer(screen, -1, 0);
 	auto pixFmt = SDL_PIXELFORMAT_RGBA32;
-	SDL_Texture *pSdlTexture = SDL_CreateTexture(sdlRender, pixFmt, SDL_TEXTUREACCESS_STREAMING,
+	SDL_Texture *pSdlTexture = SDL_CreateTexture(sdlRender, pixFmt, SDL_TEXTUREACCESS_TARGET,
 		info.cx, info.cy);
-	
+	SDL_UpdateTexture(pSdlTexture, NULL, pData, info.cx * 4);
 
-	while (1)
+	int done = 0;
+	while (!done)
 	{
-		SDL_Rect sdlRect;
-		SDL_UpdateTexture(pSdlTexture, NULL, pData, info.cx*4);
-		sdlRect.x = 0;
-		sdlRect.y = 0;
-		sdlRect.w = info.cx;
-		sdlRect.h = info.cy;
+		SDL_Event event;
+		while (SDL_PollEvent(&event) > 0)
+		{
+			switch (event.type)
+			{
+			case SDL_QUIT:
+				done = 1;
+				break;
+			}
+		}
+
 		SDL_RenderClear(sdlRender);
-		SDL_RenderCopy(sdlRender, pSdlTexture, NULL, &sdlRect);
+		SDL_Rect dest1;
+		dest1.x = 0;
+		dest1.y = 0;
+		dest1.w = info.cx/2;
+		dest1.h = info.cy/2;
+		SDL_RenderCopy(sdlRender, pSdlTexture, NULL, &dest1);
+
+		SDL_Rect dest2;
+		dest2.x = info.cx / 2;
+		dest2.y = 0;
+		dest2.w = info.cx / 2;
+		dest2.h = info.cy / 2;
+		SDL_RenderCopy(sdlRender, pSdlTexture, NULL, &dest2);
+
 		SDL_RenderPresent(sdlRender);
 
-		SDL_Delay(50);
+		SDL_Delay(10);
 	}
 
 	av_packet_unref(&packet);
