4a570f0a2d5128b41c5de16cec3a8ab89f5eb572
My Name <my-name@chromium.org>
2022-11-28 22:50:16 +0800

    add 1.simple

--- 1.simple/src/main.cpp
+++ 1.simple/src/main.cpp
@@ -1,4 +1,9 @@
 #include "window.h"
+#include <QDebug>
+
+extern "C" {
+#include "libavformat/avformat.h"
+}
 
 int main(int argc, char** argv)
 {
@@ -8,6 +13,56 @@ int main(int argc, char** argv)
 	fmt.setVersion(3, 3);
 	fmt.setProfile(QSurfaceFormat::CoreProfile);
 	QSurfaceFormat::setDefaultFormat(fmt);
+
+	AVFormatContext* fmt_ctx = avformat_alloc_context();
+	if (!fmt_ctx)
+		return -1;
+
+	const int type = 2;
+	if (1 == type) // mp4
+	{
+		QString mp4FilePath = QApplication::applicationDirPath();
+		QDir filePath(mp4FilePath);
+		int err = avformat_open_input(&fmt_ctx, QDir::toNativeSeparators(filePath.absoluteFilePath("juren-30s.mp4")).toUtf8().data(), nullptr, nullptr);
+		//char szError[256]{ 0 };
+		//av_strerror(err, szError, 256);
+		if (err < 0)
+			return -2;
+
+		qDebug() << "open success";
+		qDebug() << "filename: " << fmt_ctx->filename;
+		qDebug() << "duration: " << fmt_ctx->duration;
+		qDebug() << "nb streams: " << fmt_ctx->nb_streams;
+		for (int i = 0; i < fmt_ctx->nb_streams; ++i)
+			qDebug() << "stream codec_type: " << fmt_ctx->streams[i]->codecpar->codec_type;
+		qDebug() << "iformat name: " << fmt_ctx->iformat->name;
+		qDebug() << "iformat long name: " << fmt_ctx->iformat->long_name;
+	}
+	else if (2 == type)
+	{
+		AVDictionary* fmt_opts = nullptr;
+		av_dict_set(&fmt_opts, "probesize", "32", 0);
+
+		QString flvFilePath = QApplication::applicationDirPath();
+		QDir filePath(flvFilePath);
+		int err = avformat_open_input(&fmt_ctx, QDir::toNativeSeparators(filePath.absoluteFilePath("juren-30s.flv")).toUtf8().data(), nullptr, &fmt_opts);
+		//char szError[256]{ 0 }; 
+		av_dict_free(&fmt_opts);
+
+		if (err < 0)
+			return -2;
+
+		avformat_find_stream_info(fmt_ctx, nullptr);
+
+		qDebug() << "open success";
+		qDebug() << "filename: " << fmt_ctx->filename;
+		qDebug() << "duration: " << fmt_ctx->duration;
+		qDebug() << "nb streams: " << fmt_ctx->nb_streams;
+		for (int i = 0; i < fmt_ctx->nb_streams; ++i)
+			qDebug() << "stream codec_type: " << fmt_ctx->streams[i]->codecpar->codec_type;
+		qDebug() << "iformat name: " << fmt_ctx->iformat->name;
+		qDebug() << "iformat long name: " << fmt_ctx->iformat->long_name;
+	}
 
 	Window w;
 	w.show();
